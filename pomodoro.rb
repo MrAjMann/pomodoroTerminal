7a654edc329197c18c0c33d148bb78e22a37eb4c
MrAjMann <anthonyjmann87@gmail.com>
2020-03-21 16:41:28 +1000

    Update pomodoro.rb

--- pomodoro.rb
+++ pomodoro.rb
@@ -1,41 +1,53 @@
-require "time"
-require "TimeParser"
+# frozen_string_literal: true
+
+require 'time'
+require 'TimeParser'
 require 'tty-font'
 
 font = TTY::Font.new(:standard)
-puts font.write("Pomodoro")
+puts font.write('Pomodoro')
 
-seconds = 60 #set minute in seconds
+def seconds(_minutes)
+  seconds == 60
+end
+
 # start_time = Time.parse("%M/%S")
-print "Length of time in minutes? "
-init_time_amount = gets.chomp  + 'm'
+print 'Length of time in minutes:  '
+init_time_amount = gets.chomp + 'm'
 init_time_amount = init_time_amount.parse_time.to_i
-init_time_amount
 
-print " number of rounds"
-rounds.gets.chomp
-rounds = rounds.to_i
-rounds
+# Get the number of rounds
+print ' Number of Rounds: '
+num_rounds = gets.chomp
+num_rounds.to_i
 
-print "short break length (Recommend 5min)"
-short_break = gets.chomp + 'm'
-short_break = short_break.parse_time.to_i
-short_break
+# Short break after each round.
+print ' Short break length: '
+short_break_length = gets.chomp + 'm'
+short_break_length.parse_time.to_i
 
-print "long break length (Recommend 20min)"
-long_break = gets.chomp = 'm'
-long_break = long_break.parse_time.to_i
-long_break
+# Long break should only happen after minimum of 2 rounds
+print ' long break length: '
+long_break_length = gets.chomp + 'm'
+long_break_length.parse_time.to_i
 
+end_time = init_time_amount * seconds.to_i
 
-end_time =  init_time_amount * seconds.to_i
+start_time = 0o0
 
-
- start_time = 0
-while  start_time < end_time
-  end_time -=1
+while rounds >= 1 && start_time < end_time
+  end_time -= 1
   print end_time.to_s + "\r"
   sleep 1
-  $stdout.flush
 end
-p "times up"
+
+p 'times up'
+system(%(say -v "Oliver" "Times up have a break"))
+
+# p "Next round begins in: #{short_break_length}"
+
+# if rounds is greater then or equal too 1
+# and its not a break (long or short)
+# and end time is 0
+# user input equals init_time_amount
+#
